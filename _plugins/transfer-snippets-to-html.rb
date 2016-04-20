e086c1036157fa31593a51d263ba21cd12a78000
阎王 <yanwang.lj@alibaba-inc.com>
2016-04-21 01:38:38 +0800

    modify

---
+++ _plugins/transfer-snippets-to-html.rb
@@ -0,0 +1,24 @@
+module Jekyll
+  class UpcaseConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /^\.snippet$/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      '<meta charset="utf-8" />' +
+      '<title>Snippets | 小胡子哥的 snippet 分享</title>' +
+      '<meta name="description" content="代码收藏夹，github 管理" />' +
+      '<meta name="keywords" content="分享技术,分享生活,snippets,barretlee,小胡子哥" />' +
+      '<meta name="email" content="barret.china@gmail.com" />' +
+      '<meta name="author" content="http://weibo.com/173248656" />' +
+      content
+    end
+  end
+end
